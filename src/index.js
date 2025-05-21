eabd13656f83758fcdde0fe976d68ed716ee24bc
I Nyoman Budhiarta  Suputra <budhiarta.suputra@sadhana-corp.id>
2025-05-22 00:41:39 +0800

    fix auth

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { app } from "./application/app.js";
 import { logger } from "./application/logging.js";
 
+app.get("/", (req, res) => {
+  res.send("Halo, server berjalan dengan baik!");
+});
+
 app.listen(8000, "0.0.0.0", () => {
   logger.info("app runing at port 8000");
 });
