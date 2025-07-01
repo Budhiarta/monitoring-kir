9fb17985f0b7ce3eef11b5059640b159d57594f1
I Nyoman Budhiarta  Suputra <budhiarta.suputra@sadhana-corp.id>
2025-07-01 20:29:15 +0800

    feat: expres change to uploads

--- src/application/app.js
+++ src/application/app.js
@@ -2,15 +2,11 @@ import { errorMiddleware } from "../middleware/error-middleware.js";
 import { publicRouter } from "../routes/public-api.js";
 import express from "express";
 import cors from "cors";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export const app = express();
 app.use(express.json());
 
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+app.use("/uploads", express.static("public"));
 app.use(cors());
 app.use(publicRouter);
 app.use(errorMiddleware);
